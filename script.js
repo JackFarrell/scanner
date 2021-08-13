dcf45516e21c246665411a525d1061d61e82cf94
JackFarrell <69850422+JackFarrell@users.noreply.github.com>
2021-08-13 06:44:34 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var totalPrice = 0
-function checkPrice() {
-    const price = Math.floor(Math.random() * 2 + 1)
-    document.getElementById("price").innerHTML = price + "$"
-
-    totalPrice = totalPrice + price
-    document.getElementById("totalPrice").innerHTML = totalPrice + "$"
-}
